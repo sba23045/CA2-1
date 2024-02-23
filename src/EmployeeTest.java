63cafb1ce05ed1e4ac053192a18a23c727f4b7df
sba23045 <152573259+sba23045@users.noreply.github.com>
2024-02-23 02:53:44 +0000

    Add Code to search and display Name employee "projectGroup array"

--- src/EmployeeTest.java
+++ src/EmployeeTest.java
@@ -14,15 +14,19 @@ public class EmployeeTest {
      */
     
     public static void main(String[] args) {
-1 =     Employee Employee = Employee;
-new Employee("Joe Bloggs", "jb@gmail.com");
-        Employee 2 = new Employee("Ann Banana", "ab@gmail.com");
-        Employee 3 = new Employee("Tom Thumb", "tt@gmail.com");
+        Employee e1 = new Employee("Joe Bloggs", "jb@gmail.com");
+        Employee e2 = new Employee("Ann Banana", "ab@gmail.com");
+        Employee e3 = new Employee("Tom Thumb", "tt@gmail.com");
    
-        Employee[] projectGroup = {1, 2, 3};
+        Employee[] projectGroup = {e1, e2, e3};
         
         System.out.println("Next Employee Number: " + Employee.getNextEmpNum());
    
-   
-}
+        int m = 0;
+        for (Employee emp : projectGroup) {
+            if (emp.getEmpNum() > m) {
+                System.out.println(emp.getName());
+            }
+        }
+    }
 }
