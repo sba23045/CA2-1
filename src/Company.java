5638c0500a088c8726dd311732c8288c28545e4f
sba23045 <152573259+sba23045@users.noreply.github.com>
2024-02-23 04:05:49 +0000

    Add Construction, Overload, Implementation

--- src/Company.java
+++ src/Company.java
@@ -1,3 +1,8 @@
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -8,5 +13,34 @@
  * @author User
  */
 public class Company {
+    public String companyName;
+    public ArrayList<Employee> staff;
     
+    public Company() {
+        this("Business Gnómes Ltd");
+        this.staff = new ArrayList<>();
+    }
+    
+    public Company(String companyName) {
+        this.companyName = companyName;
+        this.staff = new ArrayList<>();
+    }
+    
+    public void addNewStaff(Employee employee) {
+        staff.add(employee);
+    }
+    
+    public int getStaffNumber() {
+        return staff.size();
+    }
+    
+    public void listEmployees(int empNumThreshold) {
+        Iterator<Employee> iterator = staff.iterator();
+        while (iterator.hasNext()) {
+            Employee emp = iterator.next();
+            if (emp.getEmpNum() > empNumThreshold) {
+                System.out.println(emp.getName());
+            }
+        }
+    }
 }
