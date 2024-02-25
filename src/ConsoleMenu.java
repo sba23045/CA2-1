1fbabc32ead68c23614236d1033eee61350b1883
sba23045 <152573259+sba23045@users.noreply.github.com>
2024-02-25 02:06:41 +0000

    Updated Console: Switch options and Default Message to the user

--- src/ConsoleMenu.java
+++ src/ConsoleMenu.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -8,5 +11,32 @@
  * @author User
  */
 public class ConsoleMenu {
+    public Company company;
+    public Scanner scanner;
     
+    public ConsoleMenu(Company company) {
+        this.company = company;
+        this.scanner = new Scanner(System.in);
+    }
+        
+        switch (option) {
+            case 1: 
+                viewStaff();
+                break;
+            case 2:
+                addNewStaff();
+                break;
+            case 3:
+                removeStaff();
+                break;
+            case 4:
+                running = false
+                break;
+            default:
+                System.out.println("Invalid option selected. PLease try again");
+                break;
+        }
+    }
 }
+
+
